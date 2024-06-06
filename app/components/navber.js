afe5c90182b2fc9f2d504c061593443c284c29da
sajibkhandev <sajibkhan.mern@gmail.com>
2024-06-06 20:13:30 +0600

    navber done

--- app/components/navber.js
+++ app/components/navber.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import Image from 'next/image'
+import Logo from './../../public/logo.png'
+
 
 const Navber = () => {
   return (
-    <div>Navber</div>
+    <div className='py-[18px]'>
+      <div className='max-w-container mx-auto'>
+      <div className='flex items-center justify-between'>
+      <Image
+      src={Logo}
+      width={80}
+      height={80}
+      alt="Picture of the author"
+    />
+      <ul className='flex gap-x-[40px]'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Service</li>
+        <li>Price</li>
+        <li>Blog</li>
+      </ul>
+    <button className='bg-blue-400 py-[14px] px-[28px]'>Contact Us</button>
+      </div>
+      </div>
+    </div>
   )
 }
 
