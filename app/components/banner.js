942ed9d286b1c67af45ab3ac390d3abedc21c537
sajibkhandev <sajibkhan.mern@gmail.com>
2024-06-10 18:27:36 +0600

    after add para

--- app/components/banner.js
+++ app/components/banner.js
@@ -15,7 +15,7 @@ const Banner = () => {
         <div className='flex justify-between items-center'>
             <div>
                 <h2 className='text-[64px] text-primary font-bold w-720 pt-200'>Professional task manager tool to grow </h2>
-                <p className='text-[18px] text-primary font-normal w-516 pt-[40px] pb-[48px]'>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime.</p>
+                <p className='relative after:absolute after:top-[28px] after:left-[0px] after:w-[6px] after:h-[80px] after:bg-[#FFC556] text-[18px] text-primary font-normal w-516 pt-[40px] pl-[20px] pb-[48px]'>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime.</p>
                 <div className='relative w-[600px]'>
                     <input className='w-full py-[28px] px-[38px]  text-white bg-[#F9F6FE] rounded-[24px]' placeholder='Join with us using your gmail address'/>
                     <button className='absolute top-[50%] -translate-y-1/2 right-[16px] bg-[#8454F5] rounded-[18px] text-[#fff] font-semibold py-[14px] px-[28px]'>Sign up</button>
